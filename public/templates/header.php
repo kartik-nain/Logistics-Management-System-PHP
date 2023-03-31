98a65857cfc6b26c3fce574e28cebdd4f99fc079
Kartik Nain <kartik.nain.9@gmail.com>
2023-03-31 13:27:28 -0400

    Revert "Merge pull request #2 from kartik-nain/hs" "Bad commit" This reverts commit a9626ff94c991402903eb324219c91490a4a0a83, reversing changes made to 97c979645b5e608045e23eab3684dd3bb3cede78.

--- public/templates/header.php
+++ public/templates/header.php
@@ -5,7 +5,8 @@
   <meta charset="utf-8" />
   <meta http-equiv="x-ua-compatible" content="ie=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link href="" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
   <title>LMS</title>
 </head>
 
